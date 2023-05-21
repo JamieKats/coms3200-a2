92cc9b02a5a354d597e3bc394c2c0061a58d9827
JamieKats <jamie.kats@hotmail.com>
2023-05-21 15:22:30 +1000

    Passing SWITCH_LOCAL2_GREETING

---
+++ tests/debug-tests/device.py
@@ -0,0 +1,102 @@
+"""
+Represents the class for a successful incoming TCP connection from a switch
+to the current switch (host).
+
+this class might be used for UDP connections also later?? just tcp for now
+"""
+    
+import socket
+import ipaddress
+from sender_receiver import SenderReceiver
+import packet as pkt
+import queue
+import time
+
+READY_PACKET_TIMEOUT = 5
+
+class Device:
+    def __init__(self, conn_socket) -> None:
+        self.conn_socket: socket.socket = conn_socket
+        self.ip: ipaddress.IPv4Address = None
+        self.latitude: int = None
+        self.longitude: int = None
+        self.euclidian_dist = None
+        self.ready_to_receive = 0
+        self.fragments = []
+        
+    # def set_ip(self, ip: ipaddress.IPv4Address):
+    #     self.ip = ip
+        
+    def set_latitude(self, latitude: int):
+        self.latitude = latitude
+        
+    def set_longitude(self, longitude: int):
+        self.longitude = longitude  
+        
+    def receive_packet(self) -> pkt.Packet:
+        return SenderReceiver.receive_packet_tcp(conn_socket=self.conn_socket)
+        
+    def send_packet(self, packet: pkt.Packet):
+        SenderReceiver.send_packet_tcp(packet=packet, conn_socket=self.conn_socket)
+        
+    def set_ready_to_receive(self):
+        """
+        Sets the ready to receive timestamp as now
+        """
+        self.ready_to_receive = time.time()
+        
+    def is_ready_to_receive(self):
+        """
+        Checks if the device is ready to receive a data packet.
+        The device is ready to receive a data packet if they have responded to 
+        a query packet with a ready packet in the last 5 seconds.
+
+        Returns:
+            _type_: _description_
+        """
+        return time.time() < (self.ready_to_receive + READY_PACKET_TIMEOUT)
+        
+class ClientDevice(Device):
+    def __init__(self, conn_socket) -> None:
+        super().__init__(conn_socket)
+        # self.client_ip: ipaddress.IPv4Address = None
+    
+class ClientSwitch(ClientDevice):
+    def __init__(self, conn_socket) -> None:
+        super().__init__(conn_socket)
+
+class ClientAdapter(ClientDevice):
+    def __init__(self, udp_socket, socket_addr) -> None:
+        super().__init__(conn_socket=udp_socket)
+        self.socket_addr = socket_addr
+        self.packet_queue = queue.Queue()
+        # self.time_last_ready_pkt: time.time = 0
+        
+    def receive_packet(self) -> pkt.Packet:
+        return SenderReceiver.receive_packet_udp(self.packet_queue)
+    
+    def send_packet(self, packet: pkt.Packet):
+        SenderReceiver.send_packet_udp(packet=packet, udp_socket=self.conn_socket, client_addr=self.socket_addr)
+        
+        
+        
+class HostSwitch(Device):
+    def __init__(self, conn_socket) -> None:
+        super().__init__(conn_socket)
+        # self.host_ip: ipaddress.IPv4Address = None
+        self.my_assigned_ip: ipaddress.IPv4Address = None
+        
+    # def set_host_ip(self, host_ip):
+    #     self.host_ip = host_ip
+        
+    def set_assigned_ip(self, assigned_ip):
+        self.my_assigned_ip = assigned_ip
+        
+# class Switch(Device):
+#     def __init__(self, conn_socket) -> None:
+#         super().__init__(conn_socket)
+    
+    
+    
+        
+        
